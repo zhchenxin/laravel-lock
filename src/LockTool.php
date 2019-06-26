9640b829acaba7216014a08ad1856dd58ac3fe62
chenxin <1577838065@qq.com>
2019-06-26 10:32:26 +0800

    add queue methods

--- src/LockTool.php
+++ src/LockTool.php
@@ -4,7 +4,7 @@ namespace Zhchenxin\Lock;
 
 use Zhchenxin\Lock\Driver\RedisLock;
 
-class LockTool implements LockInterface
+class LockTool
 {
     /**
      * @var LockInterface
@@ -18,11 +18,24 @@ class LockTool implements LockInterface
         }
     }
 
+    /**
+     * 获取锁
+     * @param string $key 锁名称
+     * @param string $requestId 一个随机数, 用于释放锁
+     * @param int $expire 过期时间,单位秒
+     * @return bool 是否获取到锁
+     */
     public function lock($key, $requestId, $expire)
     {
         return $this->driver->lock($key, $requestId, $expire);
     }
 
+    /**
+     * 释放锁
+     * @param string $key 锁名称
+     * @param string $requestId 一个随机数, 与获取锁时的参数相同
+     * @return bool 是否释放成功
+     */
     public function unlock($key, $requestId)
     {
         return $this->driver->unlock($key, $requestId);
@@ -30,9 +43,9 @@ class LockTool implements LockInterface
 
     /**
      * 获取到锁，就执行 $func, 如果没有获取到，则直接返回
-     * @param string $key
-     * @param int $expire
-     * @param callable $func
+     * @param string $key 锁名称
+     * @param int $expire 超时时间, 最好设置成业务处理的最大处理时间
+     * @param callable $func 业务处理
      * @return bool 获取锁是否成功
      * @throws
      */
@@ -50,6 +63,30 @@ class LockTool implements LockInterface
         }
 
         return true;
+    }
+
+    /**
+     * 获取到锁，就执行 $func, 如果没有获取到，等待0.1秒, 然后重新去获取锁
+     * @param $key
+     * @param $expire
+     * @param callable $func
+     */
+    public function queue($key, $expire, Callable $func)
+    {
+        while (true) {
+            $requestId = $this->generateRequestId();
+
+            if ($this->lock($key, $requestId, $expire)){
+                try {
+                    $func();
+                } finally {
+                    $this->unlock($key, $requestId);
+                }
+                return;
+            } else {
+                usleep(1000);
+            }
+        }
     }
 
     public function generateRequestId()
