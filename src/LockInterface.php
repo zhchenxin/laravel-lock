a6c38ba554fc03eb4db86b76cdf5b6c10bb3c3c2
chenxin <1577838065@qq.com>
2019-07-16 15:26:53 +0800

    注释

--- src/LockInterface.php
+++ src/LockInterface.php
@@ -4,7 +4,20 @@ namespace Zhchenxin\Lock;
 
 interface LockInterface
 {
+    /**
+     * 获取锁
+     * @param string $key         锁名称
+     * @param string $requestId   锁id
+     * @param int $expire         超时时间，单位秒
+     * @return bool 是否获取到锁
+     */
     public function lock($key, $requestId, $expire);
 
+    /**
+     * 释放锁
+     * @param string $key        锁名称
+     * @param string $requestId  锁id
+     * @return bool 是否获取到锁
+     */
     public function unlock($key, $requestId);
 }
